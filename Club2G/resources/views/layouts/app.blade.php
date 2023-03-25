b6b832357ec70d0ea3c131ac78ce74972bd3079e
mvasqueze <68928428+mvasqueze@users.noreply.github.com>
2023-03-25 16:12:18 -0500

    Laravel project

---
+++ Club2G/resources/views/layouts/app.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+<head>
+ <meta charset="utf-8" />
+ <meta name="viewport" content="width=device-width, initial-scale=1" />
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css"
+rel="stylesheet" crossorigin="anonymous" />
+ <title>@yield('title', 'Club 2G')</title>
+</head>
+<body>
+ <!-- header -->
+ <nav class="navbar navbar-expand-lg navbar-dark bg-secondary py-4">
+ <div class="container">
+ <a class="navbar-brand" href="#">Club 2G - MATT</a>
+ <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bstarget="#navbarNavAltMarkup"
+ aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+ <span class="navbar-toggler-icon"></span>
+ </button>
+ <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+ <div class="navbar-nav ms-auto">
+ <a class="nav-link active" href="#">Home</a>
+ <a class="nav-link active" href="#">About</a>
+ </div>
+ </div>
+ </div>
+ </nav>
+ <header class="masthead bg-primary text-white text-center py-4">
+ <div class="container d-flex align-items-center flex-column">
+ <h2>@yield('subtitle', 'Conoce Medellín y gana premios')</h2>
+ </div>
+ </header>
+ <!-- header -->
+ <div class="container my-4">
+ @yield('content')
+ </div>
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
+crossorigin="anonymous">
+ </script>
+</body>
+</html>
---
+++ Club2G/resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title', 'Home Page - Club 2G')
+@section('content')
+<div class="text-center">
+ ¡Bienvenido/a a Club 2G!
+</div>
+@endsection
